36a9e57a79d1de0c935f668fc91817c5281db034
Alexandr Korn <53853090+avcore@users.noreply.github.com>
2020-01-14 18:55:29 +0300

    Create i3inst.sh

---
+++ i3inst.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+pacman -S i3-wm
+pacman -S i3status dmenu xterm
+pacman -S xorg-server xf86-video-intel xf86-input-synaptics
+pacman -S slim
+echo "current_theme archlinux" >> /etc/slim.conf
+
+#echo "current_theme archlinux" && echo "xx" && echo "superline3" >> /tmp/shooter/test/s.conf
+
+(
+  echo export LANG=ru_RU.utf8;
+  echo setxkbmap "us,ru" ",winkeys" "grp:caps_toggle,grp_led:caps";
+  echo xset b off;
+  echo exec i3
+  ) >> ~/.xsession
+  
+  systemctl start slim
+  
+  pacman -S terminus-font lilyterm
