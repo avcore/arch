54d9eefff11be8718110455019bd9d9942689689
Alexandr Korn <53853090+avcore@users.noreply.github.com>
2020-08-31 10:25:09 +0300

    Update virtarch2.sh
    sergii

--- virtarch2.sh
+++ virtarch2.sh
@@ -26,7 +26,7 @@ mkinitcpio -p linux
 echo '3.5 Устанавливаем загрузчик'
 pacman -Syy
 pacman -S grub --noconfirm 
-grub-install /dev/mmcblk0
+grub-install /dev/sda
 
 echo 'Обновляем grub.cfg'
 grub-mkconfig -o /boot/grub/grub.cfg
